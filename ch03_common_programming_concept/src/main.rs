91f1ebbe518b844058c47c08522c792817bcf040
Michael Shu <michael.shu@thermovisionusa.com>
2022-07-23 23:46:30 +0800

    ch03: 3.1 Variables and Mutability.

--- ch03_common_programming_concept/src/main.rs
+++ ch03_common_programming_concept/src/main.rs
@@ -1,3 +1,41 @@
+fn immutable() {
+    let x = 5;
+    // x = 7;
+    println!("x = {}", x);
+    /*
+    error[E0384]: cannot assign twice to immutable variable `x`
+     --> src/main.rs:3:5
+      |
+    2 |     let x = 5;
+      |         -
+      |         |
+      |         first assignment to `x`
+      |         help: consider making this binding mutable: `mut x`
+    3 |     x = 7
+      |     ^^^^^ cannot assign twice to immutable variable
+     */
+}
+
+fn mutable() {
+    let mut y = 7;
+    println!("{}", y);
+    y = 8; // This is okay because the it's mutable
+    println!("{}", y);
+}
+
+pub fn shadowing() {
+    // Shadowing
+    let z = 9;
+    println!("{}", z);
+    let z = 10;
+    println!("{}", z);
+    let z = z + 1;
+    println!("{}", z);
+    let z = "shadowing";
+    println!("{}", z);
+}
 fn main() {
-    println!("Hello, world!");
+    immutable();
+    mutable();
+    shadowing();
 }
