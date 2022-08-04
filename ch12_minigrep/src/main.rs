372192a085dc4933fcf0c8a9d6651f56c0926820
Michael Shu <michael.shu@thermovisionusa.com>
2022-08-04 19:52:13 +0800

    ch12: Assign arguments to variables.

--- ch12_minigrep/src/main.rs
+++ ch12_minigrep/src/main.rs
@@ -2,6 +2,9 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
 
-    println!("{:?}", args);
+    println!("Searching for {}", query);
+    println!("In the file {}", filename);
 }
