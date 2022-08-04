51e2f87e917bbb34aee753fe44a9adc602854fcc
Michael Shu <michael.shu@thermovisionusa.com>
2022-08-04 20:04:18 +0800

    ch12: Create parse_cofig() to parse arguments.

--- ch12_minigrep/src/main.rs
+++ ch12_minigrep/src/main.rs
@@ -3,8 +3,7 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In the file {}", filename);
@@ -12,4 +11,11 @@ fn main() {
     let contents = fs::read_to_string(filename).expect("Failed to read file");
 
     println!("With text:\n {}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
