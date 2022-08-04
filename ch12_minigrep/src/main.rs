e38a7ad5e808122504841bd3f1649e24cbf0efd6
Michael Shu <michael.shu@thermovisionusa.com>
2022-08-04 20:30:55 +0800

    ch12: Create struct Config and use it accordingly.

--- ch12_minigrep/src/main.rs
+++ ch12_minigrep/src/main.rs
@@ -3,19 +3,23 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In the file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In the file {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Failed to read file");
+    let contents = fs::read_to_string(config.filename).expect("Failed to read file");
 
     println!("With text:\n {}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String,
+}
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
 
-    (query, filename)
+    Config { query, filename }
 }
