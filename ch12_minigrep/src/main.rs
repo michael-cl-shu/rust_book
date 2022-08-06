d1628e7b1d398b7b5d8fdb68cb10bbe118bba2f0
Michael Shu <michael.shu@thermovisionusa.com>
2022-08-07 00:38:53 +0800

    ch12: Refactor code to src/lib.rs and link it with ch12_minigrep::*

--- ch12_minigrep/src/main.rs
+++ ch12_minigrep/src/main.rs
@@ -1,8 +1,7 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
+use ch12_minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
@@ -13,34 +12,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In the file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = ch12_minigrep::run(config) {
         println!("Application Error: {}", e);
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename).expect("Failed to read file");
-
-    println!("With text:\n {}", contents);
-
-    Ok(())
 }
