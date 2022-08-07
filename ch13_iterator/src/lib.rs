ea8b34ac2ea24398541998f9c2601f514a353656
Michael Shu <michael.shu@thermovisionusa.com>
2022-08-07 22:32:41 +0800

    ch13: into_iter().filter().collect() to comsume iterator.

--- ch13_iterator/src/lib.rs
+++ ch13_iterator/src/lib.rs
@@ -1,9 +1,18 @@
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
 #[cfg(test)]
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
 fn list_increment(list: &Vec<u32>, inc: u32) -> Vec<u32> {
     return list.iter().map(|i| i + inc).collect();
 }
 mod tests {
-
     #[test]
     fn increment_test() {
         let list = vec![1, 2, 3];
@@ -31,5 +40,39 @@ mod tests {
         let total: i32 = v1_iter.sum();
 
         assert_eq!(total, 6);
+    }
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            crate::Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            crate::Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            crate::Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = crate::shoes_in_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                crate::Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                crate::Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                },
+            ]
+        );
     }
 }
