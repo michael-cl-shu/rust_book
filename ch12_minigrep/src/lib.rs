586c7264d19b45096cc587b302bf44ed5bdf3619
Michael Shu <michael.shu@thermovisionusa.com>
2022-08-07 23:43:40 +0800

    ch12: Use Iterator instead of Indexing. Convert Config::new parameter to an Iterator.

--- ch12_minigrep/src/lib.rs
+++ ch12_minigrep/src/lib.rs
@@ -9,13 +9,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        // Skip executible's name
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
